b2b43a5745429d36883a47b98b705092426a09df
XeniaFedulova <XJQw8hvLGTpSLeN>
2022-04-12 02:17:18 +0300

    fixes + first_valid_date

--- main.py
+++ main.py
@@ -5,6 +5,52 @@ from datetime import datetime
 from datetime import timedelta
 import argparse
 
+def make_date_of_string(date:str):
+    dates = {}
+    dates['year'], dates['month'], dates['day'] = date.split('-')
+    for key, item in dates.items():
+        if item.startswith("0"):
+            item = item.replace("0", "")
+        dates[key] = int(item)
+    date = datetime(day=dates['day'], month=dates['month'], year=dates['year'])
+    return date
+
+def search_first_valid_date(db: DataStorage, api: BTCApi, start, end):
+    try:
+        api.make_request(str(start.date()), str(start.date()))
+        return start
+    except:
+        # бинарный поиск
+        delta = (end - start) / 2
+        start += delta
+        while True:
+            if delta.days < 1:
+                try:
+                    api.make_request(str(start.date()), str(start.date()))
+                    return start
+                except:
+                    try:
+                        api.make_request(str(end.date()), str(end.date()))
+                        return end
+                    except:
+                        return None
+            try:
+                api.make_request(str(start.date()), str(start.date()))
+                end = start
+                start -= delta
+                delta = (end - start) / 2
+                start += delta
+                print(start)
+                print(end)
+                # поиск в меньшей половине
+            except:
+                delta = (end - start) / 2
+                start += delta
+                print(start)
+                print(end)
+                # поиск в большей половине
+
+#парсинг аргументов из командной строки
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('-start_date', type=str, help='input start date in format yyyy-mm-dd')
 parser.add_argument('-end_date', type=str, help='input end date in format yyyy-mm-dd')
@@ -20,82 +66,29 @@ end_date = args.end_date
 n_days = timedelta(args.n)
 first_valid_date = args.first_valid_date
 
+#объявление объектов классов
 db = DataStorage("btc_tracker")
 api = BTCApi()
 plot = plotData()
 
-dates = {}
-dates['start_year'], dates['start_month'], dates['start_day'] = start_date.split('-')
-dates['end_year'], dates['end_month'], dates['end_day'] = end_date.split('-')
 
-for key, item in dates.items():
-    if item.startswith("0"):
-        item = item.replace("0", "")
-    dates[key] = int(item)
-
-start = datetime(day=dates['start_day'], month=dates['start_month'], year=dates['start_year'])
-end = datetime(day=dates['end_day'], month=dates['end_month'], year=dates['end_year'])
+start = make_date_of_string(start_date)
+end = make_date_of_string(end_date)
 time = end - start
 amount_of_dates = time.days
 data_from_db = db.get_from_database(start_date, end_date)
 
 
-def search_first_valid_date(db: DataStorage, api:BTCApi, start, end):
-    data = db.get_from_database(start.date(), start.date())
-    if len(data) != 0:
-        data = data[0][0]
-        print("Первая валидная дата из интервала - "+data)
-        return data
-    else:
-        try:
-            data = api.make_request(start.date(), start.date())
-            i = input()
-            data = list(data.keys())[0]
-            print("Первая валидная дата из интервала - "+data)
-            return data
-        except:
-            while True:
-                print("Бинарный поиск")
-                delta = (end - start) / 2
-                start += delta
-                print(start)
-                while True:
-                    data = db.get_from_database(start.date(), start.date())
-                    if len(data) == 0:
-                        try:
-                            data = api.make_request(str(start.date()), str(start.date()))
-                            end = start
-                            start -= delta
-                            delta = (end - start) / 2
-                            start = start + delta
-                            print(start)
-                            print(end)
-                            i = input()
-                            #поиск в меньшей половине
-                        except:
-                            delta = (end - start) / 2
-                            start += delta
-                            print(start)
-                            i = input()
-                            #поиск в большей половине
-                    else:
-                        delta = (end - start) / 2
-                        start += delta
-                        print(start)
-                        i = input()
-                        #поиск в большей половине
+first_valid_date = search_first_valid_date(db, api, start, end)
 
-
-
-
-print(search_first_valid_date(db, api, start, end))
-i = input()
-
-
+if first_valid_date == None:
+    print("Заданный интервал невалиден")
+else:
+    start = datetime(day=first_valid_date.day, month=first_valid_date.month, year=first_valid_date.year)
+    print("Первая валидная дата в итервале - " + str(start.date()))
 
 
 if len(data_from_db) < amount_of_dates:
-
     start_date_req = start
 
     while True:
@@ -123,6 +116,5 @@ raw_data = db.get_from_database(start_date, end_date)
 data = {}
 for i in raw_data:
     data[i[0]] = i[1]
-
 
 plot.make_plot(data)
